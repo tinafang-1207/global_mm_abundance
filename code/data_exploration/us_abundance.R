ccb4653d63e3fa706bc4b128a56714638198bfd8
tinafang-1207 <yutianfang@ucsb.edu>
2024-07-19 18:05:13 -0700

    code update

---
+++ code/data_exploration/us_abundance.R
@@ -0,0 +1,15 @@
+
+#### clean working environment #####
+rm(list = ls())
+
+#### read in package ####
+library(tidyverse)
+library(readxl)
+
+### read in data ####
+
+data_orig <- read_xlsx("data/raw_data/species_abundance - U.S.xlsx")
+
+# clean data
+
+# 
