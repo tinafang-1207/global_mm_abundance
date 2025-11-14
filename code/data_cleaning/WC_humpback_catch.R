22b936c7cf42fe68f0b08e7e8806f695577bc6bd
tinafang-1207 <yutianfang@ucsb.edu>
2025-11-13 16:59:27 -0800

    compile catch data

---
+++ code/data_cleaning/WC_humpback_catch.R
@@ -0,0 +1,162 @@
+
+### clean working environment ###
+rm(list = ls())
+
+### read in library ###
+library(tidyverse)
+library(sf)
+
+### read in data ###
+
+# IWC individual catch data
+individual <- readRDS("/Users/yutianfang/Dropbox/ca_set_gillnet_bycatch/confidential/iwc_data/processed/individual_NP_catch_clean.Rds")
+
+# IWC summary catch data
+summary <- readRDS("/Users/yutianfang/Dropbox/ca_set_gillnet_bycatch/confidential/iwc_data/processed/summary_catch_clean.Rds")
+
+# world map
+world <- rnaturalearth::ne_countries(scale = "medium", returnclass = "sf")
+usa <- rnaturalearth::ne_states(country = "United States of America", returnclass = "sf")
+mexico <- rnaturalearth::ne_countries(country ="Mexico", returnclass = "sf")
+canada <- rnaturalearth::ne_countries(country = "Canada", returnclass = "sf")
+
+
+# identify the tour that potentially catch the interested stocks
+# 3 breeding grounds: Hawaii, Central America, Mainland Mexico
+
+# No catch found around Hawaii and Central America
+# How to judge the catch_area to be filtered with?
+# The catch areas filtered should be: USA West coast, Mexico, Br.Columbia
+# These areas likely contain the catch of humpback whale belongs to CA/OR/WA stock
+
+summary_clean <- summary%>%
+  filter(area %in% c("USA W coast", "Br.Columbia", "Mexico")) %>%
+  filter(operation_type == "Commercial") %>%
+  select(expedition_iwc, expedition_new, area) %>%
+  rename(exp_id_sum = expedition_iwc)
+
+
+# join with individual catch database
+
+ind_filtered <- individual %>%
+  mutate(expedition_new = paste(exp_id_sum, catch_year, sep = "-")) %>%
+  select(-exp_id_ind) %>%
+  select(expedition_new, exp_id_sum, everything()) %>%
+  inner_join(summary_clean, by = c("exp_id_sum", "expedition_new")) %>%
+  filter(species == "Humpback whale") %>%
+  # the whales were typically seen-off west coast from April through November (when they're feeding)
+  mutate(catching_season = ifelse(catch_month %in% c(4,5,6,7,8,9,10,11), "Feeding", "Breeding"))
+
+ind_feeding <- ind_filtered %>%
+  mutate(exp_id_sum = as.factor(exp_id_sum)) %>%
+  filter(catching_season == "Feeding") %>%
+  #filter(area == "USA W coast"|area == "Br.Columbia") %>%
+  # only keep the catch location with latitude less than 49N - keep it consistent with the survey region
+  filter(lat_dd <= 49) %>%
+  group_by(catch_year, exp_id_sum) %>%
+  mutate(total_catch = n()) %>%
+  ungroup()
+
+
+ind_breeding <- ind_filtered %>%
+  mutate(exp_id_sum = as.factor(exp_id_sum)) %>%
+  filter(catching_season == "Breeding") %>%
+  # should the US West coast be kept?
+  # filter(area == "USA W coast"|area == "Mexico") %>%
+  group_by(catch_year, exp_id_sum, area) %>%
+  mutate(total_catch = n()) %>%
+  ungroup()
+
+# There were whales caught in Br.Columbia during breeding season and whale caught in Mexico during feeding season
+
+# Compile the total catch data
+ind_feeding_sum <- ind_filtered %>%
+  mutate(exp_id_sum = as.factor(exp_id_sum)) %>%
+  filter(catching_season == "Feeding") %>%
+  # only keep the catch location with latitude less than 49N - keep it consistent with the survey region
+  filter(lat_dd <= 49) %>%
+  group_by(catch_year) %>%
+  summarize(total_catch = n())
+
+ind_breeding_sum <- ind_breeding %>%
+  mutate(exp_id_sum = as.factor(exp_id_sum)) %>%
+  filter(catching_season == "Breeding") %>%
+  # should the US West coast be kept?
+  group_by(catch_year) %>%
+  summarize(total_catch = n())
+
+catch_humpback <- rbind(ind_feeding_sum, ind_breeding_sum) %>%
+  group_by(catch_year) %>%
+  summarize(catch = sum(total_catch))
+  
+
+################################################################# 
+# Examine catch data
+
+base_theme <- theme(axis.text=element_text(size=7),
+                    axis.text.y = element_text(angle = 90, hjust = 0.5),
+                    axis.title=element_text(size=8),
+                    legend.text=element_text(size=6),
+                    legend.title=element_text(size=7),
+                    strip.text = element_text(size=8),
+                    plot.tag =element_text(size=9),
+                    plot.title=element_blank(),
+                    # Gridlines
+                    panel.grid.major = element_blank(),
+                    panel.grid.minor = element_blank(),
+                    panel.background = element_blank(),
+                    axis.line = element_line(colour = "black"),
+                    # Legend
+                    legend.key = element_rect(fill = NA),
+                    legend.background = element_rect(fill=alpha('blue', 0)))
+
+g_feeding <- ggplot(ind_feeding, aes(x = long_dd, y = lat_dd)) +
+  geom_sf(data = usa, fill = "grey85", col = "white", linewidth=0.01, inherit.aes = F) +
+  geom_sf(data = mexico, fill = "grey85", col = "white", linewidth=0.01, inherit.aes = F) +
+  geom_sf(data = canada, fill = "grey85", col = "white", linewidth=0.01, inherit.aes = F) +
+  geom_point(aes(size = total_catch, color = exp_id_sum), alpha = 0.3) +
+  coord_sf(xlim = c(-130, -105), ylim = c(20, 49)) +
+  scale_x_continuous(breaks=seq(-130, -110, 5)) +
+  scale_y_continuous(breaks=seq(20, 49, 5)) +
+  labs(x = NULL, y = NULL) +
+  facet_wrap(~ catch_year, ncol = 10) +
+  guides(color = "none") + 
+  base_theme + theme(legend.background = element_blank(),
+                     legend.key = element_blank())
+
+g_feeding
+
+
+g_breeding <- ggplot(ind_breeding, aes(x = long_dd, y = lat_dd)) +
+  geom_sf(data = usa, fill = "grey85", col = "white", linewidth=0.01, inherit.aes = F) +
+  geom_sf(data = mexico, fill = "grey85", col = "white", linewidth=0.01, inherit.aes = F) +
+  geom_sf(data = canada, fill = "grey85", col = "white", linewidth=0.01, inherit.aes = F) +
+  geom_point(aes(size = total_catch, color = exp_id_sum), alpha = 0.3) +
+  coord_sf(xlim = c(-130, -105), ylim = c(20, 49)) +
+  scale_x_continuous(breaks=seq(-130, -110, 5)) +
+  scale_y_continuous(breaks=seq(20, 49, 5)) +
+  labs(x = NULL, y = NULL) +
+  facet_wrap(~ catch_year, ncol = 5) +
+  guides(color = "none") + 
+  base_theme + theme(legend.background = element_blank(),
+                     legend.key = element_blank())
+
+g_breeding
+
+# save figure
+plotdir <- "data/confidential/iwc_catch_figure"
+
+ggsave(g_feeding, filename=file.path(plotdir, "hbk_feeding.png"), 
+              width=8, height=5, units="in", dpi=600)
+ggsave(g_breeding, filename=file.path(plotdir, "hbk_breeding.png"), 
+       width=8, height=5, units="in", dpi=600)
+
+write.csv(catch_humpback, "data/confidential/input_data/wc_humpback_catch.csv", row.names = FALSE)
+
+
+
+
+
+
+
+
--- code/data_cleaning/iwc_summary_catch.R
+++ code/data_cleaning/iwc_summary_catch.R
@@ -5,6 +5,7 @@ rm(list = ls())
 ### read in library ###
 library(tidyverse)
 library(readxl)
+library(sf)
 
 ### read in data ###
 catch_orig <- read_excel("/Users/yutianfang/Dropbox/ca_set_gillnet_bycatch/confidential/iwc_data/original/SummaryDatabaseV7.1/All-V7-2-Jun-2023.xlsx", sheet = "ExtraInfo") %>%
@@ -12,6 +13,8 @@ catch_orig <- read_excel("/Users/yutianfang/Dropbox/ca_set_gillnet_bycatch/confi
 
 area_orig <- read_excel("/Users/yutianfang/Dropbox/ca_set_gillnet_bycatch/confidential/iwc_data/original/SummaryDatabaseV7.1/All-V7-2-Jun-2023.xlsx", sheet = "Areas") %>%
   janitor::clean_names()
+
+world <- rnaturalearth::ne_countries(returnclass = "sf")
 
 # expedition_orig <- read_excel("/Users/yutianfang/Dropbox/ca_set_gillnet_bycatch/confidential/iwc_data/SummaryDatabaseV7.1/All-V7-2-Jun-2023.xlsx", sheet = "Expeditions")
 
@@ -22,15 +25,58 @@ area <- area_orig %>%
   rename(ocean = oc,
          min_lat = min_4,
          max_lat = max_5,
-         min_long = min_6,
-         max_long = max_7) %>%
+         min_lon = min_6,
+         max_lon = max_7) %>%
+  mutate(area_note = x9) %>%
   select(-code, -x8, -x9)
+
+# convert the Expedition area to a sf object
+area_clean <- area %>%
+  separate(min_lat, into = c("min_lat_val", "min_lat_dir"), sep = "(?<=\\d)(?=\\D)") %>%
+  separate(max_lat, into = c("max_lat_val", "max_lat_dir"), sep = "(?<=\\d)(?=\\D)") %>%
+  separate(min_lon, into = c("min_lon_val", "min_lon_dir"), sep = "(?<=\\d)(?=\\D)") %>%
+  separate(max_lon, into = c("max_lon_val", "max_lon_dir"), sep = "(?<=\\d)(?=\\D)") %>%
+  mutate(across(ends_with("_val"), as.numeric)) %>%
+  mutate(min_lon_val = ifelse(min_lon_dir == "E",min_lon_val, -min_lon_val),
+         max_lon_val = ifelse(max_lon_dir == "E", max_lon_val, -max_lon_val),
+         min_lat_val = ifelse(min_lat_dir == "N", min_lat_val, -min_lat_val),
+         max_lat_val = ifelse(max_lat_dir == "N", max_lat_val, -max_lat_val)) %>%
+  select(-max_lat_dir, -min_lat_dir, -max_lon_dir, -min_lon_dir) %>%
+  rename(min_lat = min_lat_val,
+         max_lat = max_lat_val,
+         min_lon = min_lon_val,
+         max_lon = max_lon_val)
+
+
+areas_clean_sf <-  do.call(
+  rbind,
+  lapply(1:nrow(area_clean), function(i) {
+    with(area_clean[i, ], {
+      st_as_sf(
+        data.frame(area = area, ocean = ocean),
+        geometry = st_sfc(
+          st_polygon(list(matrix(
+            c(min_lon, min_lat,
+              min_lon, max_lat,
+              max_lon, max_lat,
+              max_lon, min_lat,
+              min_lon, min_lat),
+            ncol = 2, byrow = TRUE
+          )))
+        ),
+        crs = 4326
+      )
+    })
+  })
+)
+  
+
 
 # clean catch data
 data <- catch_orig %>%
   # rename columns 
   rename(ocean = oc,
-         expedition_id = ex,
+         expedition_iwc = ex,
          landing_site_company = land_st_floating_factory,
          exp_start_day = x7,
          exp_start_month = start,
@@ -48,8 +94,10 @@ data <- catch_orig %>%
   mutate(end_date = as.Date(paste(exp_end_day, exp_end_month, exp_end_year, sep = "-"), format = "%d-%b-%Y")) %>%
   select(-exp_start_day, -exp_start_month, -exp_start_year, -exp_end_day, -exp_end_month, -exp_end_year, -girth, -blubber_thickness, -testes_wt, -weight, -detailed_lengths, -colour, -indiv_posns, -reference) %>%
   # combine info from area (location) 
-  left_join(area, by = c("ocean", "area")) %>%
-  select(year, ocean, area, min_lat, max_lat, min_long, max_long, start_date, end_date, everything()) %>%
+  left_join(area_clean, by = c("ocean", "area")) %>%
+  # create new expedition column
+  mutate(expedition_new = paste(expedition_iwc, year, sep = "-")) %>%
+  select(year, expedition_iwc, expedition_new, nation, ocean, area, min_lat, max_lat, min_lon, max_lon, start_date, end_date, everything()) %>%
   # clean category
   mutate(ocean = case_when(ocean == "NA"~"North Atlantic",
                            ocean == "SH"~"Southern Hemisphere",
@@ -73,9 +121,7 @@ data <- catch_orig %>%
                                     operation_type == "Cr"~"Commercial under reservation"))
 
 saveRDS(data, file = "/Users/yutianfang/Dropbox/ca_set_gillnet_bycatch/confidential/iwc_data/processed/summary_catch_clean.Rds")
-
-data_clean <- data %>%
-  filter(ocean == "North Pacific" & nation %in% c("USSR", "USA", "Japan"))
+saveRDS(areas_clean_sf, file = "/Users/yutianfang/Dropbox/ca_set_gillnet_bycatch/confidential/iwc_data/processed/catch_area_sf.Rds")
 
 
 
