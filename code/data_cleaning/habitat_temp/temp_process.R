c2908aca640da8094e88bba526d65238b32df410
tinafang-1207 <yutianfang@ucsb.edu>
2026-03-15 17:28:47 -0700

    update Oregon harbor seal habitat

--- code/data_cleaning/habitat_temp/temp_process.R
+++ code/data_cleaning/habitat_temp/temp_process.R
@@ -11,10 +11,12 @@ library(tidyterra)
 
 ### read in data ###
 sst_df <- readRDS("data/habitat_gis/sst_cobe2_us_west_coast.rds")
+sst_rast <- readRDS("data/habitat_gis/sst_rast.rds")
 usa <- rnaturalearth::ne_states(country = "United States of America", returnclass = "sf")
 mexico <- rnaturalearth::ne_countries(country = "Mexico", returnclass = "sf")
 states <- rnaturalearth::ne_states(country = "United States of America", returnclass = "sf") %>%
   st_transform(4326)
+usa_coastline <- read_sf("data/habitat_gis/tl_2025_us_coastline/tl_2025_us_coastline.shp")
 
 # cetacean habitat shapefile
 cetacean_orig <- st_read("data/habitat_gis/BIA_II/WC_WGS84.shp")
@@ -26,30 +28,7 @@ pinniped_orig <- readRDS("data/habitat_gis/cwhr_ranges_simplified.Rds")
 # The CWHR pinniped distribution is year-round
 # Sealion: Primarily breed in California but their distribution seems throughout the entire U.S.West coast
 # Northern elephant seal: Primarily breed in California in the winter months (December, January, February, March)
-#(should filter to winter months?)
 # California harbor seal: Nothing to notice, they're California stock and non-migratory
-
-# convert sst_df back to raster
-# convert dataframe to SpatRaster
-# unique months
-dates <- sort(unique(sst_df$date))
-
-# create raster for each month
-rast_list <- lapply(dates, function(d) {
-  
-  tmp <- sst_df %>%
-    filter(date == d) %>%
-    select(lon, lat, sst)
-  
-  r <- rast(tmp, type = "xyz", crs = "EPSG:4326")
-  names(r) <- as.character(d)
-  
-  r
-})
-
-# stack into one SpatRaster
-sst_rast <- rast(rast_list)
-
 
 # set up habitat for each species
 
@@ -129,6 +108,146 @@ ca_harbor_seal_orig <- pinniped_orig %>%
 northern_elephant_seal_orig <- pinniped_orig %>%
   filter(comm_name == "Northern elephant seal")
 
+################################################# Harbor seal (OR)
+
+# make sure it is in lon/lat
+usa_coastline <- st_transform(usa_coastline, 4326)
+
+# define West Coast bounding box
+west_bbox <- st_bbox(c(
+  xmin = -130,
+  xmax = -110,
+  ymin = 30,
+  ymax = 50
+), crs = st_crs(4326))
+
+west_bbox_sf <- st_as_sfc(west_bbox)
+
+# keep only coastline features that intersect the box
+usa_west_coast <- usa_coastline %>%
+  st_intersection(west_bbox_sf)
+
+# Get the oregon boundary 
+or_bbox <- st_bbox(oregon)
+ymin_or <- unname(or_bbox["ymin"])   # Oregon-California border latitude
+ymax_or <-unname(or_bbox["ymax"])
+
+oregon_coast_box <- st_as_sfc(st_bbox(c(
+  xmin = -130,
+  xmax = -120,
+  ymin = ymin_or,
+  ymax = ymax_or
+), crs = st_crs(4326)))
+
+oregon_coast <- usa_west_coast %>%
+  st_intersection(oregon_coast_box) %>%
+  st_union() %>%
+  st_transform(crs = 5070)
+
+# buffer 31 miles
+oregon_buffer_raw <- st_buffer(oregon_coast, dist = 31 * 1609.34)
+
+# convert back to lon/lat for plotting
+oregon_buffer_raw <- st_transform(oregon_buffer_raw, 4326)
+
+# 2. Remove land portion of buffer
+oregon_buffer_ocean <- st_difference(
+  st_make_valid(oregon_buffer_raw),
+  st_make_valid(oregon)
+) %>%
+  st_intersection(oregon_coast_box)
+
+# make sure both are in lon/lat
+oregon_coast <- st_transform(oregon_coast, 4326)
+oregon_buffer_ocean <- st_transform(oregon_buffer_ocean, 4326)
+
+# get Oregon coastline x/y extent
+coast_bbox <- st_bbox(oregon_coast)
+buf_bbox   <- st_bbox(oregon_buffer_ocean)
+
+# build a box that keeps only the west side of the coastline
+xmin_buf <- as.numeric(buf_bbox["xmin"])
+xmax_buf <- as.numeric(buf_bbox["xmax"])
+ymin_buf <- as.numeric(buf_bbox["ymin"])
+ymax_buf <- as.numeric(buf_bbox["ymax"])
+
+xmax_coast <- as.numeric(coast_bbox["xmax"])
+
+# build a west-side rectangle manually
+west_box <- st_polygon(list(matrix(
+  c(
+    xmin_buf, ymin_buf,
+    xmax_coast, ymin_buf,
+    xmax_coast, ymax_buf,
+    xmin_buf, ymax_buf,
+    xmin_buf, ymin_buf
+  ),
+  ncol = 2,
+  byrow = TRUE
+))) %>%
+  st_sfc(crs = st_crs(oregon_buffer_ocean)) %>%
+  st_as_sf()
+
+# keep only the left/west side of the buffer
+oregon_buffer_pacific <- st_intersection(
+  st_make_valid(oregon_buffer_ocean),
+  st_make_valid(west_box)
+)
+
+ggplot() +
+  #geom_sf(data = oregon, fill = "grey90", color = "black", linewidth = 0.3) +
+  geom_sf(data = oregon_buffer_pacific, fill = "lightblue") +
+  geom_sf(data = oregon_coast, color = "blue", linewidth = 0.5) +
+  coord_sf(
+    xlim = c(-125.5, -121),
+    ylim = c(41, 47),
+    expand = FALSE
+  ) +
+  theme_bw() +
+  labs(
+    title = "Oregon Coastline",
+    x = "Longitude",
+    y = "Latitude"
+  )
+
+#########################################################################
+
+# create a dataframe that contains the habitat information of all species
+
+blue_whale_geom <- blue_orig %>%
+  transmute(species = "Blue whale") %>%
+  mutate(season = "Feeding ground (6,7,8,9,10,11)")
+
+humpback_whale_geom <- humpback_final %>%
+  filter(region == "California/Oregon") %>%
+  transmute(species = "CA/OR Humpback whale") %>%
+  mutate(season = "Feeding ground (3,4,5,6,7,8,9,10,11)")
+
+ca_sealion_geom <- ca_sealion_orig %>%
+  transmute(species = "California sea lion") %>%
+  mutate(season = "Year-round (California)")
+
+ca_harbor_seal_geom <- ca_harbor_seal_orig %>% 
+  transmute(species = "CA Harbor seal") %>%
+  mutate(season = "Year-round (California)")
+
+northern_elephant_seal_geom <- northern_elephant_seal_orig %>%
+  transmute(species = "Northern elephant seal") %>%
+  mutate(season = "Year-round (California)")
+
+or_harbor_seal_geom <- st_as_sf(oregon_buffer_pacific) %>%
+  transmute(species = "OR harbor seal") %>%
+  rename(geometry = x) %>%
+  mutate(season = "Year-round (Oregon)") 
+
+species_habitat <- bind_rows(blue_whale_geom,
+                             humpback_whale_geom,
+                             ca_sealion_geom,
+                             ca_harbor_seal_geom,
+                             northern_elephant_seal_geom,
+                             or_harbor_seal_geom)
+
+saveRDS(species_habitat, "data/habitat_gis/species_habitat_final.rds")
 
 #########################################################
 
@@ -137,26 +256,18 @@ northern_elephant_seal_orig <- pinniped_orig %>%
 sst_month <- sst_df %>%
   filter(date == as.Date("1850-01-01"))
 
-sst_rast_month <- sst_rast <- rast(
-  sst_month[, c("lon", "lat", "sst")],
-  type = "xyz",
-  crs = "EPSG:4326"
-)
-
 ggplot() +
-  tidyterra::geom_spatraster(data = sst_rast_month) +
-  # geom_raster(data = sst_month,
-  #             aes(x = lon, y = lat, fill = sst)) +
+  geom_raster(data = sst_month, aes(x = lon, y = lat, fill = sst)) +
   geom_sf(data = usa, fill = "grey85", col = "white",
           linewidth = 0.01, inherit.aes = FALSE) +
   geom_sf(data = mexico, fill = "grey85", col = "white",
           linewidth = 0.01, inherit.aes = FALSE) +
-  geom_sf(data = humpback_split2 %>% filter(region == "California/Oregon"),
+  geom_sf(data = oregon_buffer_pacific,
           color = "red",fill = NA,alpha = 0.4, linewidth = 0.3,
           inherit.aes = FALSE) +
   coord_sf(
-    xlim = c(-126, -110),
-    ylim = c(30,48),
+    xlim = c(-126, -121),
+    ylim = c(42, 47.5),
     expand = FALSE
   ) +
   scale_fill_viridis_c(name = "SST (°C)") +
@@ -379,5 +490,27 @@ ggsave(temp_pdo, filename=file.path(plot_dir, "temp_pdo_scaled.png"),
 write.csv(temp_orig_scaled, "data/habitat_gis/sst_total_by_species_scaled.csv")
 write.csv(pdo_clean, "data/habitat_gis/pdo_scaled.csv")
 
+#############################################################################
+
+# convert sst_df back to raster
+# convert dataframe to SpatRaster
+# unique months
+# dates <- sort(unique(sst_df$date))
+# 
+# # create raster for each month
+# rast_list <- lapply(dates, function(d) {
+#   
+#   tmp <- sst_df %>%
+#     filter(date == d) %>%
+#     select(lon, lat, sst)
+#   
+#   r <- rast(tmp, type = "xyz", crs = "EPSG:4326")
+#   names(r) <- as.character(d)
+#   
+#   r
+# })
+# 
+# # stack into one SpatRaster
+# sst_rast <- rast(rast_list)
 
 
